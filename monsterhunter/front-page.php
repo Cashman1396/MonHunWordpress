f153f27ba9402c12840cdfc74b587cbc9f2c5304
Cashman1396 <Chrisashman96@gmail.com>
2021-06-15 10:18:45 -0400

    added twitter

--- monsterhunter/front-page.php
+++ monsterhunter/front-page.php
@@ -39,7 +39,10 @@
     <h2>Lorem ipsum dolor</h2>
     <h3>Lorem ipsum dolor sit amet, consectetur</h3>
 
-    <button type="button" class="btn btn-primary">Tweet</button>
+    <button type="button" class="btn btn-primary twitter"><svg xmlns="http://www.w3.org/2000/svg" width="94.877" height="77.104" viewBox="0 0 94.877 77.104">
+  <path id="Twitter" d="M98.451,19.453a38.73,38.73,0,0,1-11.18,3.064,19.5,19.5,0,0,0,8.559-10.77A38.965,38.965,0,0,1,83.47,16.474,19.481,19.481,0,0,0,50.3,34.227,55.268,55.268,0,0,1,10.18,13.889,19.49,19.49,0,0,0,16.2,39.878a19.41,19.41,0,0,1-8.817-2.436c0,.081,0,.164,0,.247A19.481,19.481,0,0,0,23,56.774a19.571,19.571,0,0,1-5.129.682,19.286,19.286,0,0,1-3.663-.35A19.486,19.486,0,0,0,32.392,70.624a39.277,39.277,0,0,1-28.818,8.06A55.082,55.082,0,0,0,33.413,87.43c35.805,0,55.382-29.66,55.382-55.383q0-1.265-.054-2.521A39.425,39.425,0,0,0,98.451,19.453Z" transform="translate(-3.574 -10.326)" fill="#fff"/>
+</svg>
+Tweet</button>
 
     <button type="button" class="btn btn-primary post facebook"> <svg xmlns="http://www.w3.org/2000/svg" width="39.374" height="84.737" viewBox="0 0 39.374 84.737">
   <path id="Facebook" d="M57.356,33.534H43.947V24.739c0-3.3,2.189-4.073,3.731-4.073H57.14V6.147L44.108,6.1c-14.467,0-17.759,10.829-17.759,17.759v9.679H17.982V48.5h8.367V90.833h17.6V48.5H55.821Z" transform="translate(-17.982 -6.096)" fill="#fff"/>
