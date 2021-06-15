ebc11337b00d51d247d7d08b55b45064a9273fef
Cashman1396 <Chrisashman96@gmail.com>
2021-06-15 09:30:21 -0400

    added svg icons

--- monsterhunter/header.php
+++ monsterhunter/header.php
@@ -16,10 +16,7 @@
 
     <button type="button" class="btn btn-primary">Tweet</button>
 
-    <button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="39.374" height="84.737" viewBox="0 0 39.374 84.737">
-  <path id="Facebook" d="M57.356,33.534H43.947V24.739c0-3.3,2.189-4.073,3.731-4.073H57.14V6.147L44.108,6.1c-14.467,0-17.759,10.829-17.759,17.759v9.679H17.982V48.5h8.367V90.833h17.6V48.5H55.821Z" transform="translate(-17.982 -6.096)" fill="#fff"/>
-</svg>
-Play Now</button>
+    <button type="button" class="btn btn-danger">Play Now</button>
 </div>
 
 </div>
